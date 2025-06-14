7150b1a17b6c6c3fb4d5cced7742670d646ae1e6
KarVarr <megatr9n@gmail.com>
2025-06-14 18:51:59 +0400

    ADDED: fixed 2 task

--- AutolatoutPracticalTasks/AutolatoutPracticalTasks/Tasks/Task2ViewController.swift
+++ AutolatoutPracticalTasks/AutolatoutPracticalTasks/Tasks/Task2ViewController.swift
@@ -11,8 +11,42 @@ import UIKit
 // The button should be centered horizontally and have a fixed distance from the label.
 // Adjust the layout to handle different screen sizes.
 final class Task2ViewController: UIViewController {
+    let button: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Tap me!", for: .normal)
+        button.backgroundColor = .red
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let label: UILabel = {
+        let label = UILabel()
+        label.text = "Label here 2!"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .orange
+        addView()
+        layoutButton()
+    }
+    
+    func addView() {
+        view.addSubview(button)
+        view.addSubview(label)
+    }
+    func layoutButton() {
+        NSLayoutConstraint.activate(
+            [
+                label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                
+                button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 20),
+            ]
+        )
     }
 }
 
